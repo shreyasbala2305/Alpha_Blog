6ece643ff0523a17f8af9562f061a613b9b4a08d
Shreyas Balapure <shreyashbalapure@gmail.com>
2025-07-09 21:16:14 +0530

    Files uploaded

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,91 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [ :show, :edit, :update, :destroy ]
+
+  # GET /users or /users.json
+  def index
+    @users = User.all
+  end
+
+  # GET /users/1 or /users/1.json
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+
+  # GET /users/1/edit
+  # def edit
+  # end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Your account information was successfully updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  # POST /users or /users.json
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+
+    # respond_to do |format|
+    #   if @user.save
+    #     format.html { redirect_to @user, notice: "User was successfully created." }
+    #     format.json { render :show, status: :created, location: @user }
+    #   else
+    #     format.html { render :new, status: :unprocessable_entity }
+    #     format.json { render json: @user.errors, status: :unprocessable_entity }
+    #   end
+    # end
+  end
+
+  # PATCH/PUT /users/1 or /users/1.json
+  # def update
+  #   respond_to do |format|
+  #     if @user.update(user_params)
+  #       format.html { redirect_to @user, notice: "User was successfully updated." }
+  #       format.json { render :show, status: :ok, location: @user }
+  #     else
+  #       format.html { render :edit, status: :unprocessable_entity }
+  #       format.json { render json: @user.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
+
+  # DELETE /users/1 or /users/1.json
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "Account and all associated articles successfully deleted"
+    redirect_to articles_path
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params.expect(:id))
+    end
+
+    # Only allow a list of trusted parameters through.
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
+end
---
+++ db/migrate/20250703210838_create_article.rb
@@ -0,0 +1,9 @@
+class CreateArticle < ActiveRecord::Migration[8.0]
+  def change
+    create_table :articles do |t|
+      t.timestamps
+      t.string :title
+      t.text :description
+    end
+  end
+end
